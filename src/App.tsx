f41af3d24201bb2f925ee7a48360f8031edbfafa
Pranav S Kurup <pranavskurup@codedeck.dev>
2020-06-23 15:59:13 +0530

    Started working on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/navigation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edited :P :D <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Development in Progress :P</h1>
+      <Navigation/>
     </div>
   );
 }
---
+++ src/components/Navigation/navigation.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+function Navigation() {
+  return <Button color="primary">Hello World</Button>;
+}
+
+export default Navigation;
